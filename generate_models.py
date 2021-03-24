a36f69dd415bbeda21f22d735e00834070e24adc
Djura Smits <djura.smits@gmail.com>
2021-03-24 14:17:20 +0100

    Created notebook to parse new fhir profile

--- generate_models.py
+++ generate_models.py
@@ -1,26 +1,44 @@
 #!/usr/bin/env python3
 
+import os
 import shutil
 from pathlib import Path
-import os
-import subprocess
 
+import fhirloader
+import fhirspec
+import settings
 
 ROOT_PATH = Path()
-FHIR_PARSER_PATH = ROOT_PATH/'fhir-parser'
+FHIR_PARSER_PATH = ROOT_PATH / 'fhir-parser'
 MAPPINGS_FILE = 'mappings.py'
 SETTINGS_FILE = 'settings.py'
 
 
+def load_fhirspec():
+    # assure we have all files
+    loader = fhirloader.FHIRLoader(settings)
+    spec_source = loader.load(force_download=False, force_cache=False)
+
+    return fhirspec.FHIRSpec(spec_source, settings)
+
+
+# TODO: I'm following the instructions of the fhir-parser readme on how to integrate fhir parser into the project.
+#  However, this approach makes for a very confusing package structure, especially now we're calling the fhirspec
+#  module directly instead of running generate.py. Think about integrating fhir-parser in a cleaner way.
 def main():
     # Copy mappings and settings to fhir-parser submodule
-    shutil.copy(ROOT_PATH/MAPPINGS_FILE, FHIR_PARSER_PATH/MAPPINGS_FILE)
+    shutil.copy(ROOT_PATH / MAPPINGS_FILE, FHIR_PARSER_PATH / MAPPINGS_FILE)
     shutil.copy(ROOT_PATH / SETTINGS_FILE, FHIR_PARSER_PATH / SETTINGS_FILE)
 
     os.chdir(FHIR_PARSER_PATH)
 
-    # I don't like to call generate as subprocess but here we are
-    subprocess.run('./generate.py')
+    # assure we have all files
+    loader = fhirloader.FHIRLoader(settings)
+    spec_source = loader.load(force_download=False, force_cache=False)
+
+    spec = fhirspec.FHIRSpec(spec_source, settings)
+
+    spec.write()
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -1,7 +1,4 @@
 from Default.settings import *
 
-# Override with custom settings
-
-
 
 from mappings import *
