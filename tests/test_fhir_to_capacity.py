975a775f28015e2008270feb16168c097868014f
Djura Smits <djura.smits@gmail.com>
2021-05-18 16:38:35 +0200

    Added id to tests

--- tests/test_fhir_to_capacity.py
+++ tests/test_fhir_to_capacity.py
@@ -5,7 +5,9 @@ from capacity_mapping import mapping
 def test_map_patient_maps_gender():
     patient = Patient()
     patient.gender = 'male'
+    patient.id = '123'
 
     mapped = mapping.map_patient(patient)
 
     assert mapped['sex'] == 1
+    assert mapped['subjid'] == '123'
