735befeb644e46ccb4996d5ac7b621d67d144190
Djura Smits <djura.smits@gmail.com>
2021-05-19 13:30:44 +0200

    Added encounters to fill_server

--- scripts/fill_server.py
+++ scripts/fill_server.py
@@ -7,8 +7,10 @@ of resources on a fhir server that can be used to test the mappings.
 import random
 
 from clize import run
+from fhirclient.models.encounter import Encounter
 from fhirclient.models.patient import Patient
 from fhirclient.server import FHIRServer
+from fhirclient.models.fhirreference import FHIRReference
 
 from capacity_mapping.codebook import Capacity
 
@@ -27,7 +29,13 @@ def fill_server(fhir_base, n=DEFAULT_NUM_PATIENTS):
     patients = create_patients(n)
 
     for p in patients:
-        p.create(fhir_server)
+        result_json = p.create(fhir_server)
+
+        created_patient = Patient.with_json(result_json)
+
+        encounter = create_encounter(created_patient)
+
+        encounter.create(fhir_server)
 
 
 def create_patients(num_patients):
@@ -35,6 +43,17 @@ def create_patients(num_patients):
         p = Patient()
         p.gender = random.choice(list(Capacity.sex.mapping.keys()))
         yield p
+
+
+def create_encounter(patient: Patient) -> Encounter:
+    patient_reference = FHIRReference()
+    patient_reference.reference = patient.relativePath()
+    encounter = Encounter()
+    encounter.subject = patient_reference
+    encounter.status = random.choice(['planned', 'arrived', 'triaged',
+                                      'in-progress', 'onleave', 'finished',
+                                      'cancelled'])
+    return encounter
 
 
 if __name__ == '__main__':
